8616704dac7c1fb673490877380eabe61d8d802c
smaitra006 <smaitra006@gmail.com>
2025-05-23 05:28:31 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,14 @@
+console.log(2 == 2) // true
+console.log(2 < 2) // false
+console.log("2" == 2) // true 
+console.log("2" === 2) // false , checks witthout conversion , checks with the original datatype
+console.log("2" > 1) // true
+
+console.log(null == 0) // false
+console.log(null > 0) // false
+console.log(null >= 0) // true
+
+// Always use same datatype during comparision . Typescript wont even allow such bad comparisons with different datatypes
+
+
+
