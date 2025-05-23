02578c0874b6784ad7a023935ebb0f16a383da76
smaitra006 <smaitra006@gmail.com>
2025-05-23 04:52:47 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "Subhrajit" // string
+let age = 20 // number
+let married = false // boolean
+let income = null; // null -> standalone value epresenting empty
+let state; // undefined 
+// symbols -> uniqueness
+
+console.log(typeof null); // type is object
+console.log(typeof undefined); // type is undefined
+
